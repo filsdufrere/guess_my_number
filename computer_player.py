00ce31bf66dff1ea3a80f6fee0fac02a1ab805c4
Teva Brotherson <teva.brotherson@epfl.ch>
2020-10-10 18:24:19 +0200

    Ajout de computer_player.py 100 % aléatoire

---
+++ computer_player.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 18:10:44 2020
+
+@author: tevab
+"""
+
+
+import random 
+
+from guess_my_number import MIN, MAX, GuessMachine
+
+if __name__ == 'main':
+    guess_machine = GuessMachine()
+    while True:
+        attempt = random.randint(MIN,MAX)
+        result = guess_machine.guess(attempt)
+        print('tried %d : %s' % (attempt, result))
+        if result == 'found':
+            print('Finished in %d attempts.' % guess_machine.number_of_attempt)
+            break
